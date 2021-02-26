7b5cfb361172f1361d160ed4896c1502a9aec094
Robert <55710848+Rakancito@users.noreply.github.com>
2021-02-25 18:38:38 -0600

    Update player_shop.sql

--- DB/player_shop.sql
+++ DB/player_shop.sql
@@ -7,7 +7,7 @@ DROP TABLE IF EXISTS `player_shop`;
 CREATE TABLE `player_shop` (
   `shop_id` int(32) NOT NULL AUTO_INCREMENT,
   `owner_id` int(32) NOT NULL DEFAULT '0',
-  `owner_name` varchar(16) NOT NULL,
+  `owner_name` varchar(32) NOT NULL,
   `map_index` int(16) NOT NULL DEFAULT '0',
   `x` int(32) NOT NULL DEFAULT '0',
   `y` int(32) NOT NULL DEFAULT '0',
@@ -15,7 +15,7 @@ CREATE TABLE `player_shop` (
   `date_close` varchar(40) NOT NULL DEFAULT '0',
   `race` int(32) NOT NULL DEFAULT '0',
   `count` int(32) NOT NULL DEFAULT '0',
-  `name` varchar(16) NOT NULL DEFAULT '',
+  `name` varchar(64) NOT NULL DEFAULT '',
   `channel` int(16) NOT NULL DEFAULT '1',
   `gold` int(32) NOT NULL DEFAULT '0',
   `status` smallint(4) NOT NULL DEFAULT '1',
